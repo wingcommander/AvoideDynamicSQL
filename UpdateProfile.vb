cf9c072b121cea0c5f53d961e2361e7540e6b9b1
wingcommander <garvanjones@gmail.com>
2019-10-04 11:10:37 +0100

    no message

---
+++ UpdateProfile.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class Admin_Configure_User_Profile
+
+    Protected Sub btnSave_OnClick(sender As Object, e As EventArgs)
+       
+        Dim countries As String = String.Join(", ", From item In lstCountryAssignments.Items Select item.Value)
+
+        sqlData_UserProfile.UpdateParameters("Countries").DefaultValue = countries
+
+        sqlData_UserProfile.Update()
+
+
+    End Sub
+
+
+End Class
